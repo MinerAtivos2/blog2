f185996dc3b8ed961bafb34fe67ff0e44a519af3
MinerAtivos2 <banri32800@outlook.com>
2025-10-29 14:00:44 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  base: "/blog-investimentos/", // ⚠️ MUDE PARA O NOME DO SEU REPO
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/blog2/" : "/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
-```
-
-**tsconfig.json**
-```json
-{
-  "files": [],
-  "references": [
-    { "path": "./tsconfig.app.json" },
-    { "path": "./tsconfig.node.json" }
-  ],
-  "compilerOptions": {
-    "baseUrl": ".",
-    "paths": {
-      "@/*": ["./src/*"]
-    }
-  }
-}
+}));
